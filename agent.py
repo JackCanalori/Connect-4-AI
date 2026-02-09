0c7a99f0c1cfed5d53aee1b74bca307aa218eb9e
jack <rj.canalori@gmail.com>
2026-02-09 14:35:48 -0500

    initial file setup

---
+++ agent.py
@@ -0,0 +1,14 @@
+class Agent:
+
+    ai = '' # the agent's "game piece" either X or O for text or yellow or red for visual
+    human = ''# the human's "game piece" either X or O for text or yellow or red for visual
+    def __init__(self, agent, player):
+        ai = agent
+        human = agent
+
+
+    def can_play_column(self, column):
+        pass
+
+    def play(self, state):
+        pass
